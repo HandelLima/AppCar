ee491f61de96542b18da93a982e6dfdfb2b73d79
HandelLima <handel.nlima@gmail.com>
2023-02-23 22:09:10 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/appcar/SplashWindow.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appcar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_window);
+    }
+}
